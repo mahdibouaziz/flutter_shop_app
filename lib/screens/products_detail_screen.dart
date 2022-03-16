1eabcbc631d678fe03b20306f839dbd96c9eb146
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-03-16 09:55:18 +0100

    added product details page

--- lib/screens/products_detail_screen.dart
+++ lib/screens/products_detail_screen.dart
@@ -18,6 +18,38 @@ class ProductDetailScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(loadedProduct.title)),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              loadedProduct.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            '\$${loadedProduct.price}',
+            style: const TextStyle(color: Colors.grey, fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
