a354a566c755f2a5fb4617b80969d7cd715c1739
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-03-17 10:10:24 +0100

    added the functionality to delete a product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -62,4 +62,9 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String productId) {
+    _items.removeWhere((element) => element.id == productId);
+    notifyListeners();
+  }
 }
--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/providers/products.dart';
 import 'package:flutter_shop_app/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -31,7 +33,9 @@ class UserProductItem extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.delete),
             color: Theme.of(context).errorColor,
-            onPressed: () {},
+            onPressed: () {
+              Provider.of<Products>(context, listen: false).deleteProduct(id);
+            },
           ),
         ]),
       ),
